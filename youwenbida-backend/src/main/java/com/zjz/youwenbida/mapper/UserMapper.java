deba664a3c360809a709a6cf5dddca6eab7cb69e
Zhang <2751628691@qq.com>
2024-06-25 15:00:43 +0800

    build: 项目初始化
    项目初始化

---
+++ youwenbida-backend/src/main/java/com/zjz/youwenbida/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.zjz.youwenbida.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjz.youwenbida.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
